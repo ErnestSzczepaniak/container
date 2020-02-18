c5063bcc28794322330b2e11cee08ae4322dd1d3
en2 <ernest.szczepaniak.en2@gmail.com>
2020-02-18 20:28:22 +0100

    dodanie klasy iterable

--- include/container_generic.h
+++ include/container_generic.h
@@ -20,6 +20,9 @@ public:
     bool is_empty();
     bool is_full();
 
+    virtual int index_front() = 0;
+    virtual int index_back() = 0;
+
 protected:
     int _size_actual;
     int _size_max;
--- include/container_queue.h
+++ include/container_queue.h
@@ -9,13 +9,13 @@
 */
 
 #include "container_generic.h"
-#include "container_iterator.h"
+#include "iterable.h"
 
 #define __index_front ((_head >= _size_actual) ? _head - _size_actual : _head + size - _size_actual)
 #define __index_back (_head != 0 ? _head - 1 : size - 1)
 
 template<typename T = int, int size = 32>
-class Container_queue : public Container_generic
+class Container_queue : public Container_generic, public Iterable<Container_queue<T, size>, T>
 {
 public:
     Container_queue();
@@ -24,18 +24,12 @@ public:
     void push(const T & item);
     void pop();
 
-    const T & front();
-    const T & back();
-
-    Iterator<T, size> begin() { return {_item, __index_front, false};}
-    Iterator<T, size> end() { return {_item, _head, true};}
-
-protected:
-    
+    const T & at(int index);
+    int index_front() {return __index_front;}
+    int index_back() {return __index_back;}
 
 private:
     T _item[size];
-
     int _head;
 
 }; /* class: Container_queue */
@@ -71,15 +65,9 @@ void Container_queue<T, size>::pop()
 }
 
 template<typename T, int size>
-const T & Container_queue<T, size>::front()
+const T & Container_queue<T, size>::at(int index)
 {
-    return _item[__index_front];
-}
-
-template<typename T, int size>
-const T & Container_queue<T, size>::back()
-{
-    return _item[__index_back];
+    return _item[(__index_front + index) % size];
 }
 
 #endif /*define: container_queue_h*/
